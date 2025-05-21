6658c96144c19a01126e95b8adc519eeaf002760
Akvin <126989925+Akvin0@users.noreply.github.com>
2025-05-22 02:52:58 +0300

    Add files via upload

---
+++ docs/html/search/all_3.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['length_0',['length',['../struct_c_string.html#ade888bbc6fcd32934f19a356b05ba458',1,'CString']]]
+];
---
+++ docs/html/struct_c_string.js
@@ -0,0 +1,7 @@
+var struct_c_string =
+[
+    [ "capacity", "struct_c_string.html#a2377d47d55238bfca8fc7d3bac54ac57", null ],
+    [ "cs", "struct_c_string.html#ae553799f514f039e2c88e1ee2cc2815f", null ],
+    [ "data", "struct_c_string.html#a8ec0a381ff57fd82c341a71d2ce0f822", null ],
+    [ "length", "struct_c_string.html#ade888bbc6fcd32934f19a356b05ba458", null ]
+];
