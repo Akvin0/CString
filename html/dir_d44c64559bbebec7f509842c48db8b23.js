2189c720a83eae46e753944eee63322bbfc36399
Akvin <126989925+Akvin0@users.noreply.github.com>
2025-05-22 03:34:33 +0300

    Add files via upload

---
+++ html/dir_d44c64559bbebec7f509842c48db8b23.js
@@ -0,0 +1,4 @@
+var dir_d44c64559bbebec7f509842c48db8b23 =
+[
+    [ "cstr.h", "cstr_8h.html", "cstr_8h" ]
+];
---
+++ html/search/all_2.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['invalid_0',['invalid',['../cstr_8h.html#a806f153ea99a469ab7b8bf260df0ca8e',1,'cstr.h']]]
+];
